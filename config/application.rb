9e2f064ca5a2f23e04d2ab258f0f974754b305d1
Jason <jason.addie@gmail.com>
2012-11-14 17:18:40 +0400

    added tbilisi timezone and i18n settings

--- config/application.rb
+++ config/application.rb
@@ -27,11 +27,17 @@ module BootstrapStarter
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
+    config.time_zone = 'Tbilisi'
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
+    config.i18n.default_locale = :ka
+
+    # rails will fallback to config.i18n.default_locale translation
+    config.i18n.fallbacks = true
+
+    # rails will fallback to en, no matter what is set as config.i18n.default_locale
+    config.i18n.fallbacks = [:ka]
 
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
