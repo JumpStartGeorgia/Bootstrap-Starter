f24bdc049d74293a290ac790aef579025ca60d9f
jason <jason.addie@gmail.com>
2015-02-16 15:31:32 +0400

    upgrading gem and fixed js

--- Gemfile
+++ Gemfile
@@ -37,7 +37,7 @@ group :assets do
   gem "therubyracer"
   gem 'less-rails', git: 'git://github.com/metaskills/less-rails.git'
   gem 'twitter-bootstrap-rails', :git => 'git://github.com/seyhunak/twitter-bootstrap-rails.git'  , branch: 'bootstrap3'
-  gem 'jquery-datatables-rails', '~> 2.2.3'
+  gem 'jquery-datatables-rails', '~> 3.1.1'
   gem "jquery-ui-rails" , "~> 4.1.2"  
 end
 
--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -6,32 +6,16 @@ $(document).ready(function(){
 
 
   $('#users-datatable').dataTable({
+    "processing": true,
     "serverSide": true,
     "ajax": $('#users-datatable').data('source'),
     "order": [[4, 'desc']],
     "language": {
-      "url": gon.datatable_i18n_url
+     "url": gon.datatable_i18n_url
     },
     "columnDefs": [
-       { orderable: false, targets: [-1] }
+      { orderable: false, targets: [-1] }
     ]
-    /*,
-    
-
-    "sDom": "<'row'<'col-sm-6'l><'col-sm-6'f>r>t<'row'<'col-sm-6'i><'col-sm-6'p>>",    
-    "sPaginationType": "bootstrap",
-    "bJQueryUI": true,
-    "bProcessing": true,
-    "bServerSide": true,
-    "bDestroy": true,
-    "bAutoWidth": false,
-    "sAjaxSource": $('#users-datatable').data('source'),
-    "oLanguage": {
-      "sUrl": gon.datatable_i18n_url
-    },
-    "aaSorting": [[2, 'desc']]
-    */
   });
-
 
 });
